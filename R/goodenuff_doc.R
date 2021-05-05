2bdcede4371789d91a16e109b09cac61351499cc
Martin Jean <44728169+martinjeanphd@users.noreply.github.com>
2021-05-05 11:35:50 -0400

    Update goodenuff_doc.R
    French localization of file names and text

--- R/goodenuff_doc.R
+++ R/goodenuff_doc.R
@@ -8,17 +8,17 @@
 goodenuff_doc <- function() {
   fs::dir_create("docs")
 
-  fs::file_create("docs/notebook.md")
+  fs::file_create("docs/carnet.md")
 
-  notebook_content <- "# Notebook\n\n...a running lab notebook describing various ideas for the project and how these were implemented\n\n\n\n\nsource: https://swcarpentry.github.io/good-enough-practices-in-scientific-computing/"
-  readr::write_lines(x = notebook_content, file = "docs/notebook.md")
+  notebook_content <- "# Carnet\n\n...un cahier de travail décrivant les différentes idées du projet et la manière dont elles ont été mises en œuvre.\n\n\n\n\nsource: https://swcarpentry.github.io/good-enough-practices-in-scientific-computing/"
+  readr::write_lines(x = notebook_content, file = "docs/carnet.md")
 
-  fs::file_create("docs/manuscript.md")
-  manuscript_content <- "# Manuscript\n\n...a running draft of a manuscript describing the project findings\n\n\n\n\nsource: https://swcarpentry.github.io/good-enough-practices-in-scientific-computing/"
-  readr::write_lines(x = manuscript_content, file = "docs/manuscript.md")
+  fs::file_create("docs/manuscrit.md")
+  manuscript_content <- "# Manuscrit\n\n...une version préliminaire du manuscrit décrivant les résultats du projet.\n\n\n\n\nsource: https://swcarpentry.github.io/good-enough-practices-in-scientific-computing/"
+  readr::write_lines(x = manuscript_content, file = "docs/manuscrit.md")
 
 
-  fs::file_create("docs/changelog.txt")
-  changelog_content <- "...make dated notes about changes to the project in this file in reverse chronological order (i.e., most recent first). This file is the equivalent of a lab notebook, and should contain entries like those shown below.\n\n## 2016-04-08\n\n* Switched to cubic interpolation as default.\n\n* Moved question about family's TB history to end of questionnaire.\n\n## 2016-04-06\n\n* Added option for cubic interpolation.\n\n* Removed question about staph exposure (can be inferred from blood test results).\n\n\n\n\nsource: https://swcarpentry.github.io/good-enough-practices-in-scientific-computing/"
-  readr::write_lines(x = changelog_content, file = "docs/changelog.txt")
+  fs::file_create("docs/Journal.txt")
+  changelog_content <- "...inscrivez dans ce fichier des notes datées sur les modifications apportées au projet, dans l'ordre chronologique inverse (c'est-à-dire en commençant par les plus récentes). Ce fichier est l'équivalent d'un cahier de laboratoire et doit contenir des entrées comme celles présentées ci-dessous.\n\n\n\n\nsource: https://swcarpentry.github.io/good-enough-practices-in-scientific-computing/"
+  readr::write_lines(x = changelog_content, file = "docs/journal.txt")
 }
